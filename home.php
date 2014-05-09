13df8d820622bbbafc4b0d3a99e398b5eee61014
Boitumelo phaho <boitumelophaho@yahoo.com>
2014-05-09 13:53:27 +0200

    Update home.php

--- home.php
+++ home.php
@@ -3,42 +3,62 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="External.css" />
-<title>Welcome</title>
+<title>Welcome to C&T Bags</title>
 <script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+ <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet">
+        <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
 
-<div img src="j.jpg">
-<div id="box1">
-<align="left"><img src="WP_002011.jpg" width="350" height="260" alt="" align="right"/>
-<td style="padding-left:0px" title ="LOGIN ">
+
+<div align = "right" id="box1">
+    <!-- Begin FusionBot.com site search engine code -->
+<form name="fbsearch" method="get" action="http://ss779.fusionbot.com/b/q" style="margin:0;">
+  <div align="right"><font size="2" face="arial"><a href="http://www.fusionbot.com" target="_blank">Search C&T Bags</a>:</font><br />
+  <input type="text" name="keys" size="15">
+  <input type="submit" value="GO"><br />
+  </div>
+</form> 
+<!-- End FusionBot.com site search engine code -->
 <a href="products.php" title="Buy " style="background-image:url(cart.jpg);width:140px;height:140px;display:block;">Buy<br/></a></td><p id="mainnav">
      
-     <p>Have an account?</p>
-       <div id="button">
-<!-- Begin FreeButtons.org -->
-<style>#web-buttons-idn5pm2 a{display:block;color:transparent;} #web-buttons-idn5pm2 a:hover{background-position:left bottom;}a#web-buttons-idn5pm2a {display:none}</style>
-<table id="web-buttons-idn5pm2" width=0 cellpadding=0 cellspacing=0 border=0><tr>
-<td style="padding-left:0px" title ="LOGIN ">
-<a href="UserLogin.php" title="LOGIN " style="background-image:url(btn5pm2.png);width:90px;height:34px;display:block;"><br/></a></td>
-<td style="padding-left:0px" title ="SIGN UP">
-<a href="UserRegister.php" title="SIGN UP" style="background-image:url(bth5pm2.png);width:90px;height:34px;display:block;"><br/></a></td>
-</tr></table><a id="web-buttons-idn5pm2a" href="http://freebuttons.org">Start Button Hover by FreeButtons.org v2.0</a>
-<!-- End FreeButtons.org -->
-     
-  
-     </p>
-    <h1><font size="50" color = "yellow" >C&T BAGS</font></h1>
+    <h1> C&T Bags</h1>
 </div>
+<div align = "center" id="box1">
 <div align="right">
   <div class="main">
   <table width="1053" border="0">
   <tr>
-   
-   <div id="FormatNavBar">
-        <p align="right"><a href="home.php" target="_top">Home</a> || <a href="userLogin.php">Login</a>|| <a href="about.php" target="_top">About Us</a> || <a href="contact.php">Contact Us || <a href="products.php">Gallery </a></p>
+   <div class="navbar navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container">
+ 
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+      <!-- Be sure to leave the brand out there if you want it shown -->
+      
+	  <a class="brand" href="userLogin.php">Login</a>
+	  <a class="brand" href="UserRegister.php">Register</a>
+	  <a class="brand" href="products.php">Gallery</a>
+	  <a class="brand" href="contact.php">Contact Us</a>
+	  <a class="brand" href="about.php">About Us</a>
+ 
+      <!-- Everything you want hidden at 940px or less, place within here -->
+      <div class="nav-collapse collapse">
+        <!-- .nav, .navbar-search, .navbar-form, etc -->
       </div>
+ 
+    </div>
+  </div>
+</div>
     <?php
 	
         $pictures = array('213446_3_1.jpg','237250_4_1.jpg','260641_3_1.jpg','258103_3_1.jpg','274325_2_1.jpg','271289_2_1.jpg','270005_1_3.jpg');
@@ -46,13 +66,6 @@
 		
 		for($i = 0; $i<1;$i++ )
 		{
-	 //$pictures = new SimpleImage();
-     //$pictures->load($_FILES['uploaded_image']['tmp_name']);     
-    
-     //$pictures->save('./images/photo'.$a.'.jpg');
-     //body("Location: people.php");
-     //exit; 
-	        //$pictures[$i]->resize(300, 300);
 			echo "<td align=\"center\"><img src=\" ";
 			echo $pictures[$i];
 			echo "\"/></td>";
@@ -61,7 +74,7 @@
 	?>
    
   </div>
-  <div align="left">
+  <div align="center">
 
     <td width="747"><div class="main"></div>
       <div align="center">
@@ -90,7 +103,6 @@
    
   <table width="1032" border="0">
     <tr>
-      <td width="377" height="130"><a href="http://www.facebook.com"><img src="f.jpg" width="95" height="99" /></a></td>
       </table>
  
       <footer>
